0a77f54879ec79b1db13fe317d709eb9b8effdab
TemaSaur <temasaur@gmail.com>
2024-12-10 22:07:49 +0500

    add my applications, fix join filter logic

--- backend/applications/routes.py
+++ backend/applications/routes.py
@@ -15,6 +15,7 @@ from auth.depends import get_user
 
 
 router = APIRouter()
+vac_router = APIRouter()
 
 
 def get_status(status: int) -> str:
@@ -25,7 +26,7 @@ def get_status(status: int) -> str:
     ][status]
 
 
-@router.post("/")
+@vac_router.post("/")
 def create(
         vac_slug: str,
         app: ApplicationCreate,
@@ -37,16 +38,24 @@ def create(
         .where(Vacancy.slug == vac_slug)\
         .get()
 
+    print(vacancy_db.title)
+
     try:
-        Application\
-            .get(Application.vacancy == vacancy_db and Application.user == user)
+        h = Application\
+            .get((Application.vacancy == vacancy_db) & (Application.user == user))
+
+        print(h)
+        print(h.message, h.vacancy)
         raise HTTPException(
             403, detail="Can't send multiple applications to one vacancy"
         )
     except DoesNotExist:
         pass
 
-    resume_db = Resume.get(Resume.slug == app.resume_slug)
+    try:
+        resume_db = Resume.get(Resume.slug == app.resume_slug)
+    except DoesNotExist:
+        raise HTTPException(400, "No such resume")
 
     Application(
         user=user,
@@ -70,28 +79,44 @@ def create(
     )
 
 
+@vac_router.get("/")
+def get_vac(vac_slug: str, user=Depends(get_user)) -> ApplicationGet:
+    return get(user, vac_slug)
+
+
 @router.get("/")
-def get(vac_slug: str, user=Depends(get_user)):
+def get_my(user=Depends(get_user)) -> list[ApplicationGet]:
+    return get(user)
+
+
+def get(user: User, vac_slug: str | None = None):
+    vac_predicate = True if vac_slug is None else Vacancy.slug == vac_slug
     try:
         app_db = Application\
             .select(Application, Vacancy, Organization)\
             .join(Vacancy, on=(Application.vacancy == Vacancy.id))\
             .join(Organization)\
             .join(Resume, on=(Application.resume == Resume.id))\
-            .where(User.id == user and Vacancy.slug == vac_slug)\
-            .get()
+            .where((Application.user == user) & (vac_predicate))
+        if vac_slug:
+            app_db = app_db.get()
     except DoesNotExist:
         raise HTTPException()
 
-    vacancy = app_db.vacancy.__data__
-    vacancy['organization'] = OrganizationGet(
-        **app_db.vacancy.organization.__data__
-    )
+    def get_application(app_db):
+        vacancy = app_db.vacancy.__data__
+        vacancy['organization'] = OrganizationGet(
+            **app_db.vacancy.organization.__data__
+        )
 
-    return ApplicationGet(
-        user=UserGet(**user.__data__),
-        vacancy=VacancyGet(**vacancy),
-        resume_slug=app_db.resume.slug,
-        message=app_db.message,
-        status=get_status(app_db.status)
-    )
+        return ApplicationGet(
+            user=UserGet(**user.__data__),
+            vacancy=VacancyGet(**vacancy),
+            resume_slug=app_db.resume.slug,
+            message=app_db.message,
+            status=get_status(app_db.status)
+        )
+
+    if vac_slug:
+        return get_application(app_db)
+    return [get_application(a) for a in app_db]
--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from auth.routes import router as auth_router
 from organizations.routes import router as org_router
 from vacancies.routes import router as vac_router, org_router as org_vac_router
 from resumes.routes import router as res_router
-from applications.routes import router as app_router
+from applications.routes import router as app_router, vac_router as vac_app_router
 
 
 app = FastAPI()
@@ -17,10 +17,13 @@ def index():
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(org_router, prefix="/organizations", tags=["Organizations"])
 app.include_router(vac_router, prefix="/vacancies", tags=["Vacancies"])
+app.include_router(res_router, prefix="/resumes", tags=["Resumes"])
+app.include_router(app_router, prefix="/applications", tags=["Applications"])
 app.include_router(
     org_vac_router,
     prefix="/organizations/{org_slug}/vacancies",
     tags=["Vacancies"])
-app.include_router(res_router, prefix="/resumes", tags=["Resumes"])
 app.include_router(
-    app_router, prefix="/vacancies/{vac_slug}/applications", tags=["Applications"])
+    vac_app_router,
+    prefix="/vacancies/{vac_slug}/applications",
+    tags=["Applications"])
