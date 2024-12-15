4cf07d84eef197a84ef25184b83de1fe5bcfb94f
TemaSaur <temasaur@gmail.com>
2024-12-15 18:26:12 +0500

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,14 @@ from applications.routes import router as app_router, vac_router as vac_app_rout
 
 app = FastAPI()
 
+origins = [
+    'http://127.0.0.1',
+    'http://localhost:3000'
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
